b802ae8ac2dbb51cc907acb4c12070a19ad700fa
Andrew Hase <a.hase@bornhorstward.com.au>
2017-10-11 14:30:02 +1000

    seperating the scripts because I cannott figure out how to combine them

---
+++ ibInfoDoc.js
@@ -0,0 +1,44 @@
+// Here starts Infodoc Search
+
+function formHandler2(event) {
+    
+        event.preventDefault();
+    
+        goInfodoc();
+    
+    }
+    
+    function infodocProcessing() {
+        var input = document.getElementById("infodoc");
+    
+        input.onclick = function () {
+    
+            input.value = '';
+    
+        }
+    }
+    
+    var form = document.getElementById('infodocForm');
+    
+        form.onsubmit = formHandler2;
+    
+    
+    function goInfodoc() {
+    
+        var jobNumberInput = document.getElementById('infodoc');
+    
+        var baseHref = location.protocol + '//bwonline/infodoc/start.aspx?ProjectId=';
+    
+        var extHref = jobNumberInput.value;
+    
+    
+        if (extHref == '') {
+    
+            alert("You can't search using blank field, this makes it pretty hard for \n the program to figure out what you want to search =)");
+    
+        } else {
+    
+            window.open(baseHref + extHref);
+        }
+    }
+    infodocProcessing();
--- ibsearchAll.js
+++ ibsearchAll.js
@@ -9,13 +9,17 @@ function formHandler1(event) {
 
     event.preventDefault();
 
+
+
     goProcess();
+
 
 }
 
 
 //getting the value from the input
 function jobProcessing() {
+    
 
     //console.log("hello");
 
@@ -24,6 +28,7 @@ function jobProcessing() {
     input.onclick = function () {
 
         input.value = '';
+       
 
     }
     var form = document.getElementById('jobProcessForm');
@@ -32,26 +37,32 @@ function jobProcessing() {
 }
 //processing the value and adding the url search. 
 function goProcess() {
-    
+
     var jobNumberInput = document.getElementById('process');
     //used location.protocol so that if we change everything to https it will still work. 
     var baseHref = location.protocol + '//bwonline/JobProcess/Default.aspx?id=';
-    
+
     var extHref = jobNumberInput.value;
-    
-   // console.log(jobNumberInput + " Base: " + baseHref + " ext: " + extHref);
-    
+
+    // console.log(jobNumberInput + " Base: " + baseHref + " ext: " + extHref);
+
     //console.log('Hello Wolrd');
-    
+
     if (extHref == '') {
-    
+
         alert("You can't search using blank field, this makes it pretty hard for \n the program to figure out what you want to search =)");
 
     } else {
-    
+
         window.open(baseHref + extHref);
     }
 }
 
 //So it turns out I wasn't calling the function. 
 jobProcessing();
+
+
+
+
+
+
